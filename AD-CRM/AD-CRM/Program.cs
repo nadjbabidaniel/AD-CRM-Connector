657c5c1cebf46d9270309576e086b7162fb2af37
Daniel <danielnadjbabi@gmail.com>
2016-09-12 15:31:47 +0200

    Changed to Local connection, added config file --------------------------------------------------------------------

--- AD-CRM/AD-CRM/Program.cs
+++ AD-CRM/AD-CRM/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace AD_CRM
 {
@@ -15,50 +16,42 @@ namespace AD_CRM
         {
             //DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://93.184.191.103/xRMServer03.a24xrmdomain.info", @"A24XRMDOMAIN\Danijel.Nadjbabi", "Por4Xae3", AuthenticationTypes.Secure);
 
+            XmlDocument doc = new XmlDocument();
+            string Xml_Path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            Xml_Path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Xml_Path), "XmlData\\" + "ConfigFile.xml");
+            doc.Load(Xml_Path);
+
+            XmlNodeList xnList = doc.SelectNodes("/settings/organizations/organization/@adgroup");
+            foreach (XmlNode item in xnList)
+            {
+                Console.WriteLine(item.Value);
+            }
+
             try
             {
-                //var directoryEntry = new DirectoryEntry("LDAP://93.184.191.103/CN=Users;DC=A24XRMDOMAIN");
-                //directoryEntry.Username = @"A24XRMDOMAIN\Danijel.Nadjbabi";
-                //directoryEntry.Password = "Por4Xae3";
-                //var test = directoryEntry.Properties;
-                //DirectorySearcher ds = new DirectorySearcher(directoryEntry);
-                //ds.SearchScope = SearchScope.Subtree;
-                //ds.Filter = "(&(objectClass=group))";
-                //var objSearchResults = ds.FindAll();
-                //Console.WriteLine("Hello World: {0}", objSearchResults.ToString());
-                //Console.ReadKey();
+                DirectoryEntry directoryEntry;
 
+                String ldapPath = "LDAP://XRMSERVER02.a24xrmdomain.info";
 
-                DirectoryEntry directoryEntry = new DirectoryEntry("LDAP://a24xrmdomain.info");
-                directoryEntry.Path = "LDAP://OU=ComData,OU=Extern,OU=A24-UsersAndGroups,DC=a24xrmdomain,DC=info";
-                Console.WriteLine("Hello World: {0}", directoryEntry.Children.ToString());
-                Console.WriteLine("Hello World: {0}", directoryEntry.Guid.ToString());
-                Console.WriteLine("Hello World: {0}", directoryEntry.Name.ToString());
+                directoryEntry = new DirectoryEntry(ldapPath, @"A24XRMDOMAIN\Danijel.Nadjbabi", "Por4Xae3");
 
-                Console.WriteLine();
+                //// Search AD to see if the user already exists.
+                DirectorySearcher search = new DirectorySearcher(directoryEntry);
+                search.Filter = "(&(objectClass=user))";
 
-                using (DirectorySearcher ds = new DirectorySearcher(directoryEntry))
+                search.Filter = String.Format("(sAMAccountName={0})", "Gast");
+                search.PropertiesToLoad.Add("samaccountname");
+                search.PropertiesToLoad.Add("memberOf");
+                SearchResult result = search.FindOne();
+
+                if (result != null)
                 {
-                    ds.PropertiesToLoad.Add("name");
-                    ds.PropertiesToLoad.Add("userPrincipalName");
-
-                    ds.Filter = "(&(objectClass=user))";
-
-                    SearchResultCollection results = ds.FindAll();
-                    Console.WriteLine(results.Count);
-
-                    foreach (SearchResult result in results)
-                    {
-                        Console.WriteLine("{0} - {1}",
-                            result.Properties["name"][0].ToString(),
-                            result.Properties["userPrincipalName"][0].ToString());
-                    }
-                }
-
-
-                Console.ReadKey();
-
-
+                    // Use the existing AD account.
+                    DirectoryEntry userADAccount = result.GetDirectoryEntry();
+                    Console.WriteLine(userADAccount.Properties["memberOf"].Value);
+                    Console.ReadKey();
+                }              
+            
             }
             catch (Exception e)
             {

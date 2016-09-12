52429192c7441145149a51c4fd50c1f04b667c8b
Andrea Borbelj <borbeljandrea@gmail.com>
2016-09-12 13:37:44 +0200

    Connect to AD from local

--- AD-CRM/AD-CRM/Program.cs
+++ AD-CRM/AD-CRM/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.DirectoryServices;
 
 namespace AD_CRM
 {
-    class Program
+  internal class Program
+  {
+    private static void Main (string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+      DirectoryEntry directoryEntry;
+
+      String ldapPath = "LDAP://XRMSERVER02.a24xrmdomain.info";
+
+      directoryEntry = new DirectoryEntry (ldapPath, @"A24XRMDOMAIN\Andrea.Borbelj"
+      , "Nue5Gbe9");
+
+      //// Search AD to see if the user already exists.
+      DirectorySearcher search = new DirectorySearcher (directoryEntry);
+      //search.SearchScope = SearchScope.Subtree;
+      search.Filter = "(&(objectClass=user))";
+
+      search.Filter = String.Format ("(sAMAccountName={0})", "Gast");
+      search.PropertiesToLoad.Add ("samaccountname");
+      search.PropertiesToLoad.Add ("memberOf");
+      SearchResult result = search.FindOne ();
+
+      if (result != null)
+      {
+        // Use the existing AD account.
+        DirectoryEntry userADAccount = result.GetDirectoryEntry ();
+        Console.WriteLine (userADAccount.Properties["memberOf"].Value);
+      }
     }
+  }
 }

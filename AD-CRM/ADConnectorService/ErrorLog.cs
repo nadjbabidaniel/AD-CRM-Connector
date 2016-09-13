eaa73b8a65fa8350bc971c5cfdb68af3e2eafca6
Andrea Borbelj <borbeljandrea@gmail.com>
2016-09-13 15:44:54 +0200

    Initial Windows Service created

---
+++ AD-CRM/ADConnectorService/ErrorLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ADConnectorService
+{
+  public static class ErrorLog
+  {
+    public static void WriteErrorLog (Exception ex)
+    {
+      StreamWriter streamWriter;
+      try
+      {
+        streamWriter = new StreamWriter (AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+        streamWriter.WriteLine (DateTime.Now + ": " + ex.Source.Trim () + "; " + ex.Message.Trim ());
+        streamWriter.Flush ();
+        streamWriter.Close ();
+      }
+      catch
+      {
+      }
+    }
+
+    public static void WriteErrorLog (string message)
+    {
+      StreamWriter streamWriter;
+      try
+      {
+        streamWriter = new StreamWriter (AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+        streamWriter.WriteLine (DateTime.Now + ": " + message);
+        streamWriter.Flush ();
+        streamWriter.Close ();
+      }
+      catch
+      {
+      }
+    }
+  }
+}
---
+++ AD-CRM/ADConnectorService/Scheduler.cs
@@ -0,0 +1,37 @@
+﻿
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ADConnectorService
+{
+  public partial class Scheduler : ServiceBase
+  {
+    private Timer timer;
+
+    public Scheduler ()
+    {
+      InitializeComponent ();
+    }
+
+    protected override void OnStart (string[] args)
+    {
+      timer = new Timer ();
+     timer.Interval = 30000; //every 30 secs
+      timer.Elapsed += timer_Tick;
+      timer.Enabled = true;
+      ErrorLog.WriteErrorLog ("Test window service started");
+    }
+
+    private void timer_Tick (object sender, ElapsedEventArgs e)
+    {
+      //Write code here to do some job depends on your requirement
+      ErrorLog.WriteErrorLog ("Timer ticked and some job has been done successfully");
+    }
+
+    protected override void OnStop ()
+    {
+      timer.Enabled = false;
+      ErrorLog.WriteErrorLog ("Test window service stopped");
+    }
+  }
+}
